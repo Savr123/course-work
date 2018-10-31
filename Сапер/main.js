e1d6e961fe8bceada6960c655f3fa4610f22d01a
Savr123 <Sangievv@mail.ru>
2018-10-31 21:00:32 +0300

    31.10.18
    Work that done on lessons

--- Сапер/main.js
+++ Сапер/main.js
@@ -1,11 +1,15 @@
+'use strict';
+
 class gameArea{
 
-  constuctor(){
+  constructor(){
     this.bombs=0; //number of bombs
     this.areaSizeX=8;//size of gameSpace for x coordinate
     this.areaSizeY=8;//size of gameSpace for y coordinate
-    this.IdCell=0;
-    this.Id='Minesweeper';
+    this.IdXCell=0;
+    this.IdYCell=0;
+    this.Id='Sapper';
+    this.matrix=new Array;
   }
   getBombsNum(){//get for bomb number
     return this.bombs;
@@ -28,32 +32,49 @@ class gameArea{
   setId(Id){
     this.Id=Id;
   }
+  bombPlant(){
+    //
+  }
+  matrixCells(){
+    for (i=0;i<this.areaSizeY;i++){
+      this.matrix=new Array
+    }
+  }
   creation_area(){
     var area=document.getElementById(this.Id);
     var tb=document.createElement('table');
-    var num=0//number of bombs that already placed on the map
+    var num=1//number of bombs that already placed on the map
 
-    for(var i=0;i<=this.areaSizeY;i++){//height of table
+    for(var i=0;i<this.areaSizeY;i++){//height of table
       var tr=document.createElement('tr');
       tb.appendChild(tr);
-      for(var j=0;j<=this.areaSizeX;j++){//width of table
+      this.IdYCell++;
+      for(var j=0;j<this.areaSizeX;j++){//width of table
         var cell=new Cell;//create new obj Cell
         num++;//an increase of bomb number that already placed
-        cell.Id=this.IdCell;
-        this.IdCell++;
-        cell.onclick=function(){alert(cell.status)};//put the bomb inside Cell
-      cell.createCell(tr);
+        cell.IdY=this.IdYCell;
+        cell.IdX=this.IdXCell;
+        this.IdXCell++;
+        //Creating cell with or without bomb WHATEVER
+        cell.createCell(tr);
       }
     }
+    //Put bomb inside the cell
+    cell.bombPlace(tb);
     area.appendChild(tb);
   }
+  //
 }
 class Cell extends gameArea{
 
-   __constuctor(){
-    this.color='#fff';
+  constructor(){
+    super();
+    this.IdY=0;
+    this.IdX=0;
+    this.color='#000';
     this.size='8px';
     this.status=0;
+    this.cursor="pointer";
   }
   setCellStatus(status){
     this.status=status;
@@ -67,12 +88,56 @@ class Cell extends gameArea{
   setCellColor(color){
     this.color=color;
   }
+
   getCellColor(){
     return this.color;
   }
   createCell(tr){
     var td=document.createElement('td');
+    td.style.backgroundColor=this.color;
+    td.style.cursor=this.cursor;
     tr.appendChild(td);
+    this.IdX++;
+
+    var t=this;
+    td.onclick=function(){
+      alert(t.status);
+      alert(t.num);
+    }
+  }
+  explosion(){
+  }
+  //here we jst spawn bomb inside bombObj
+  bombspawn(tb){
+    var x,y;
+    //alert(tb);
+    //while (m>0){
+      //if (this.IdX==x && this.IdY==y && this.status!=1){
+        this.status=1;
+        alert(x);
+        alert(this.status);
+        m=0;
+      //}
+    //}
+  }
+  // here we will put the bomb inside the html cell element
+  bombPlace(){
+    var x,y;
+    var tb2=document.getElementsByTagName('table');
+    var tb1=tb2[0];
+    var m=1;
+    while(m!=0){
+      x=Math.floor(Math.random()*6);
+      y=Math.floor(Math.random()*6);
+      //if (this.IdX==x && this.IdY==y && this.status!=1){
+        this.status=1;
+        alert(x);
+        alert(this.status);
+        m=0;
+      //}
+    }
+  }
+  bombcheck(){
 
   }
 }
